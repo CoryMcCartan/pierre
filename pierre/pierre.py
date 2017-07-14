0fd78e1e8aa43cc86abeff5fb6e4c106a9f48100
Cory McCartan <cory.mccartan@gmail.com>
2017-07-14 12:27:52 -0700

    update command added

--- pierre/pierre.py
+++ pierre/pierre.py
@@ -1,12 +1,24 @@
-import os
+#!/usr/bin/env python3
 
+import os
+from datetime import datetime
+
+import yaml
 import click as cl
 
 file_arg = cl.argument("file", type=cl.File("r+"))
 desc_arg = cl.option("-d", "--description", prompt=True, default="")
 
-divider_small = "-" * 30
-divider = "#" * 78
+yaml_opts = {
+        "default_flow_style": False,
+        "width": 72,
+        "indent": 4,
+        }
+
+
+###################################
+# ENTRY POINT                     #
+###################################
 
 @cl.group()
 @cl.version_option(message="""%(prog)s version %(version)s
@@ -23,31 +35,98 @@ def main(ctx):
     pass
 
 
+###################################
+# COMMANDS                        #
+###################################
+
 @main.command()
 @cl.argument("filename", type=cl.Path())
 @cl.option("-t", "--title", prompt=True)
+@cl.option("-f", "--force", is_flag=True, help="Force overwrite any existing file.")
 @desc_arg
-def create(filename, title, description):
+def create(filename, title, force, description):
     """Create a hypothesis file."""
-    if os.path.isfile(filename):
+    if os.path.isfile(filename) and not force:
         cl.confirm(f"This will overwrite {filename}. Continue?", abort=True)
-    
+
+    doc = {
+            "title": title,
+            "description": description,
+            "date": f"{datetime.now():%B %d, %Y}",
+            "hypotheses": {},
+            "updates": [],
+            }
     with open(filename, "w+") as f:
-        writeln(title, f)
-        writeln(divider_small, f)
-        writeln(description, f)
-        writeln(divider, f)
+        yaml.dump(doc, f, **yaml_opts)
 
 @main.command()
 @file_arg
-@cl.option("-i", "--id", prompt=True, help="A short identifier for the hypothesis.")
+@cl.option("-i", "--id", prompt=True, help="A short identifier for the hypothesis. "
+        "This is how you will refer to this hypothesis in future updates.")
 @cl.option("-n", "--name", prompt=True, help="A human-readable name for the hypothesis.")
 @desc_arg
 @cl.option("-p", "--prob", type=cl.FLOAT, prompt=True, 
         help="The prior probability of the hypothesis.")
 def add(file, id, name, description, prob):
     """Add a hypothesis."""
+    doc = yaml.load(file)
+    doc["hypotheses"][id] = {
+        "name": name,
+        "description": description,
+        "prior": prob,
+    }
+    save(doc, file)
+
+
+@main.command()
+@file_arg
+@cl.option("-n", "--name", prompt=True, help="A name for the event.")
+@desc_arg
+@cl.option("-p", "--prob", type=cl.FLOAT, prompt=True, 
+        help="The probability of the hypothesis.")
+@cl.option("-l", "--likelihood", multiple=True, type=cl.Tuple([str, float]),
+        help="The likelihood of each hypothesis: P(hypothesis|envent). "
+        "Type <id> followed by <prob>.")
+def update(file, name, description, prob, likelihood):
+    """Update hypotheses based on new information."""
+    likelihood = list(likelihood)
+    doc = yaml.load(file)
+    hyp = doc["hypotheses"]
+    # prompt for missing likelihoods
+    given_hyp = [l[0] for l in likelihood]
+    to_prompt = [h for h in hyp if h not in given_hyp] 
+    for h in to_prompt:
+        likelihood.append((h, cl.prompt(f"P({name} | {h})", type=cl.FLOAT)))
+
+    # calculate bayes rule
+    if len(doc["updates"]) == 0: # first update
+        priors = {h: hyp[h]["prior"] for h in hyp}
+    else:
+        priors = doc["updates"][-1].updated.copy()
+
+    update_obj = {
+        "name": name,
+        "description": description,
+        "date": f"{datetime.now():%B %d, %Y}",
+        "priors": priors,
+        "updated": priors.copy(),
+    }
+
+    for h in hyp:
+        lk = [l[1] for l in likelihood if l[0] == h][0]
+        update_obj["updated"][h] = update_obj["priors"][h] * lk / prob
+    doc["updates"].append(update_obj)
+
+    save(doc, file)
+        
+
     
+@main.command()
+@file_arg
+def rerun(file):
+    """Rerun the document and calculations."""
+    doc = yaml.load(file)
+    save(doc, file)
 
 
 @main.command()
@@ -61,9 +140,15 @@ def help(ctx, topic):
         cl.echo(main.commands[topic].get_help(ctx))
 
 
-######################################
-# HELPER FUNCTIONS                   #
-######################################
+###################################
+# HELPER FUNCTIONS                #
+###################################
 
-def writeln(text, f):
-    print(cl.wrap_text(text), file=f)
+def save(obj, file):
+    file.seek(0)
+    file.truncate()
+    yaml.dump(obj, file, **yaml_opts)
+
+
+if __name__ == "__main__":
+    main()
